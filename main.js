561f43baf50c7818a7993c16b2b14cbf07d1ca81
Raishav Hanspal <rhanspal25@gmail.com>
2025-01-14 00:32:50 +0530

    update hack

--- main.js
+++ main.js
@@ -7,11 +7,22 @@ const sound = new Howl({
 
 // click to hear people cheer for you
 function cheerForMe() {
-    unlockHowlerAudio();
+    // unlockHowlerAudio();
     sound.play();
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("click", () => {
+        if (Howler.ctx.state === "suspended") {
+            Howler.ctx.resume().then(() => {
+                console.log("Howler audio context unlocked!");
+            }).catch((err) => {
+                console.error("Error unlocking Howler audio context:", err);
+            });
+        } else {
+            console.log("Howler audio context is already unlocked.");
+        }
+    }, { once: true });
     document.getElementById("cheer").addEventListener("click", cheerForMe);
 })
 
